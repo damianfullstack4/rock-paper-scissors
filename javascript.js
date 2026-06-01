8d7d4f75e2d83dc24387f888ba8b0a98347fdd6e
Damian <damianfullstack@gmail.com>
2026-06-01 17:41:54 -0400

    Add player and computer choice functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+console.log(getComputerChoice());
+
+console.log(getHumanChoice());
+
+function getComputerChoice(){
+    let choice = Math.random();
+
+    if(choice < (1/3)){
+        return "rock";
+    } else if(choice > (2/3)){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    return prompt("Enter rock, paper or scissors: ")
+}
