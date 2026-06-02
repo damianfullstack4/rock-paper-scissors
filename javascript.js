fa3786aa5c9ddb815d5f509c8ae2afee80929533
Damian <damianfullstack@gmail.com>
2026-06-01 20:21:47 -0400

    Add play game function to call upon round function

--- javascript.js
+++ javascript.js
@@ -1,46 +1,56 @@
+// Basic Javascript Rock Paper Scissors Browser Game //
 let computerScore = 0, humanScore = 0;
+playGame();
+console.log("Finals Scores:\nPlayer Score: " + humanScore + "  Computer Score: " + computerScore);
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
+function playGame(){
+    let humanChoice = null;
+    let computerChoice = null;
+    
+    for(let i = 0; i < 5; i++){
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+        
+    }
 
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
 
-function getComputerChoice(){
-    let choice = Math.random();
+        if(humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
+            console.log("Invalid choice, aborting round...")
+            return;
+        }
 
-    if(choice < (1/3)){
-        return "rock";
-    } else if(choice > (2/3)){
-        return "paper";
-    }else{
-        return "scissors";
+        if (humanChoice === computerChoice){
+            console.log("Tie! " + humanChoice + " ties with " + computerChoice + ".")
+            return;
+
+        }else if((humanChoice == "rock" && computerChoice == "scissors")||(humanChoice == "paper" && computerChoice == "rock")||(humanChoice == "scissors" && computerChoice == "paper")){
+            console.log("Player wins! " + humanChoice + " beats " + computerChoice + ".")
+            humanScore++;
+            return;
+
+        }else{
+            console.log("Computer wins! " + computerChoice + " beats " + humanChoice + ".")
+            computerScore++;
+            return;
+        }
+    }
+    function getComputerChoice(){
+        let choice = Math.random();
+
+        if(choice < (1/3)){
+            return "rock";
+        } else if(choice > (2/3)){
+            return "paper";
+        }else{
+            return "scissors";
+        }
+    }
+
+    function getHumanChoice(){
+        return prompt("Enter rock, paper or scissors\nPlayer Score: " + humanScore + "  Computer Score: " + computerScore);
     }
 }
 
-function getHumanChoice(){
-    return prompt("Enter rock, paper or scissors: ")
-}
-
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-
-    if(humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
-        console.log("Invalid choice, aborting round...")
-        return;
-    }
-
-    if (humanChoice === computerChoice){
-        console.log("Tie! " + humanChoice + " ties with " + computerChoice + ".")
-        return;
-
-    }else if((humanChoice == "rock" && computerChoice == "scissors")||(humanChoice == "paper" && computerChoice == "rock")||(humanChoice == "scissors" && computerChoice == "paper")){
-        console.log("Player wins! " + humanChoice + " beats " + computerChoice + ".")
-        humanScore++;
-        return;
-
-    }else{
-        console.log("Computer wins! " + computerChoice + " beats " + humanChoice + ".")
-        computerScore++;
-        return;
-    }
-}
